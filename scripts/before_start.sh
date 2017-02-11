7e3d9cd89d758f280d022e202208206faf3b1476
Artem Veikus <artem@veikus.com>
2017-02-11 14:30:44 +0200

    start scripts improvements

--- scripts/before_start.sh
+++ scripts/before_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This file need to be executed as root
 
-ifconfig wlan0 up && dhclient wlan0 && sleep 15
+ifconfig wlan0 up && sleep 15 && dhclient -1 wlan0
 
 bash /home/a3/a3-metro-train/scripts/send_stats.sh
 bash /home/a3/a3-metro-train/scripts/update.sh
