ef0f3df815e9d4c89cabf231530c9d32529bdc96
Artem Veikus <artem@veikus.com>
2017-03-11 18:10:38 +0200

    Clean up (wifi connects automatically)

--- scripts/before_start.sh
+++ scripts/before_start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # This file need to be executed as root
 
-ifconfig wlan0 up && sleep 15 && dhclient -1 wlan0
-
 bash /home/a3/a3-metro-train/scripts/send_stats.sh
 bash /home/a3/a3-metro-train/scripts/update.sh
 
