2f8df9a2224fbe19fb691bb50bddd2d0eeab01ae
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2017-04-07 22:45:26 +0300

    Fixed animations on details screen

--- FE/js/screen-station-details.js
+++ FE/js/screen-station-details.js
@@ -173,10 +173,10 @@
 
                 transferScreens[transferScreen].forEach(function(transferOption, i) {
                     if (transferOption[lang].length === 1) {
-                        $(transferTitles.get(i)).hide();
+                        $(transferTitles.get(i)).scrollText('');
                         $(transferNames.get(i)).scrollText(transferOption[lang][0]);
                     } else {
-                        $(transferTitles.get(i)).show().scrollText(transferOption[lang][0]);
+                        $(transferTitles.get(i)).scrollText(transferOption[lang][0]);
                         $(transferNames.get(i)).scrollText(transferOption[lang][1]);
                     }
                 });
